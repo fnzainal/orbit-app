ae317e0b98cd005d58767e50d6a72577945be51f
fnzainal <fnzaina@gmail.com>
2016-09-06 20:27:57 +0700

    first commit

---
+++ app/src/main/java/id/zainalfahrudin/orbit/FirstActivity.java
@@ -0,0 +1,33 @@
+package id.zainalfahrudin.orbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }00
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
